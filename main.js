d5f99dcdb10152eeadd7597e7d466b2c2789e0c5
Aaditya-web <76609534+Aaditya-web@users.noreply.github.com>
2021-11-21 18:45:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+canvas id= "getCanvas";
+getContext ("2d");
+
+function preload(){
+
+}
+
+function setup(){
+
+}
+
+
+
+function draw(){
+
+}
+
+function onClick(){
+    save("myFilteredImage.png")
+}
